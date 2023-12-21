6731f7783e1ed9c50fdba2e17215fc0e5d672ec8
huylq98 <quochuy.2.10.98@gmail.com>
2023-12-21 01:42:30 -0600

    Initial commit

---
+++ grocery-crud-be/src/main/java/vn/com/huylq/grocerycrudbe/GroceryCrudBeApplication.java
@@ -0,0 +1,13 @@
+package vn.com.huylq.grocerycrudbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryCrudBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryCrudBeApplication.class, args);
+    }
+
+}
---
+++ grocery-crud-be/src/test/java/vn/com/huylq/grocerycrudbe/GroceryCrudBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.huylq.grocerycrudbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryCrudBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
